4b6609791410f46c148410083010333e4478e5ba
“Danyal <danyalkhanzada89@gmail.com>
2021-03-05 15:52:43 -0500

    Debugger-check

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,9 +14,8 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-  // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
-
+  while(enemyHealth > 0) {
+    
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
@@ -66,8 +65,13 @@ var fight = function(enemyName) {
     window.alert("You need to pick a valid option. Try again!");
   }
 };
+}
+
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
+  debugger;
   fight(enemyNames[i]);
 }
+
+// fight()
